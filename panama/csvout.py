6a036086b20afe5b13f9c5ff03fd8579a102786d
Dan O'Huiginn <daniel@ohuiginn.net>
2013-05-03 10:17:39 +0100

    csv dumps for david

--- panama/csvout.py
+++ panama/csvout.py
@@ -28,7 +28,7 @@ def rough_guide():
     f = codecs.open('/tmp/panamacsv', 'w', 'utf-8')
     writer = UnicodeCSVWriter(f)
 
-    allpersons = Person.select()[100000:150000] 
+    allpersons = Person.select()[100000:150000]
     for person in allpersons:
         row = [person.name]
         relatedcompanies = set()
@@ -63,6 +63,52 @@ def company_dump():
         if count % 1000 == 0:
             print(count)
     f.close()
+
+
+def full_company_dump():
+    '''
+    Name of the offshore; Director 1; Director 2; Director 3; Year of incorporation; Subscriber 1; Subscriber 2; Agent;  Numero de ficha
+    '''
+    f = codecs.open('/tmp/companytable.csv', 'w', 'utf-8')
+    writer = UnicodeCSVWriter(f)
+    count = 0
+    for company in Company.select()[:100]:
+        data = [company.name]
+        data.append(unicode(company.registerdate.year if company.registerdate else None))
+        data.append(company.agent[0].name)
+        data.append(unicode(company.recordid))
+        for director in company.directors:
+            data.append(unicode(director.name))
+        for subscriber in company.subscribers:
+            data.append(unicode(subscriber.name))
+        writer.writerow(data)
+        count += 1
+        if count % 1000 == 0:
+            print(count)
+    f.close()
+
+
+def full_company_second_dump():
+    '''
+    Name of the offshore; All the names of the directors (in one tab); Year of incorporation; Subscriber 1; Subscriber 2; Agent;  Numero de ficha
+    '''
+    f = codecs.open('/tmp/companytable_2.csv', 'w', 'utf-8')
+    writer = UnicodeCSVWriter(f)
+    count = 0
+    for company in Company.select():
+        data = [company.name]
+        data.append(u', '.join(unicode(x.name) for x in company.directors))
+        data.append(unicode(company.registerdate.year if company.registerdate else None))
+        for subscriber in company.subscribers[:2]:
+            data.append(unicode(subscriber.name))
+        data.append(unicode(company.agent[0].name))
+        data.append(unicode(company.recordid))
+        writer.writerow(data)
+        count += 1
+        if count % 1000 == 0:
+            print(count)
+    f.close()
+
 
 
 if __name__ == '__main__':
